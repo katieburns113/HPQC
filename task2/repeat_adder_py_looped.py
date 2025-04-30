1783a9fa1087e6da8ff297f1ec937595b98a014a
katieburns113 <154621977+katieburns113@users.noreply.github.com>
2025-04-30 05:01:54 +0100

    Update repeat_adder_py_looped.py

--- task2/repeat_adder_py_looped.py
+++ task2/repeat_adder_py_looped.py
@@ -7,7 +7,6 @@ except:
     print("Please enter two integers as command-line arguments.")
     sys.exit(1)
 
-# Simulate heavier load
 repeat = 10_000_000  # Repeat the addition this many times
 total = 0
 
