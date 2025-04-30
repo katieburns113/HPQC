d884cefb30d1ecad5a1cb2e87b528d4c11ad63f2
Katie Burns <katie.burns6@mail.dcu.ie>
2025-04-30 06:25:07 +0100

    Task 3 Part 1: Added hello_mpi source and benchmarking outputs

---
+++ task3/hello_mpi.c
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Print Hello message
+    printf("Hello from process %d of %d\n", world_rank, world_size);
+
+    // Finalize the MPI environment
+    MPI_Finalize();
+    return 0;
+}
